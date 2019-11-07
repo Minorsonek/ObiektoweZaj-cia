0137a9294308192df63321bbd225de6259cf2c18
Patryk Mikulski <pmikulsk@mops.uci.agh.edu.pl>
2019-11-07 13:48:33 +0100

    Working API

--- Zaj5Indico/src/Main.java
+++ Zaj5Indico/src/Main.java
@@ -1,7 +1,33 @@
+import io.indico.Indico;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        var apiKey = "c368c941ab2289043cc2b60e6c35d9d4";
+
+        var filePath = "foto/bocian.jpg";
+
+        try
+        {
+            File file = new File(filePath);
+
+            BufferedImage image = ImageIO.read(file);
+
+            Indico indico = new Indico(apiKey);
+            var result = indico.fer.predict(image);
+
+            System.out.println(result.getFer());
+        }
+        catch (Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+
         System.out.println("Hello world");
     }
 }
